808455d3279bce2b8f90d0476243c143cdec9c3b
SnIvan <ivan@Devastator.local>
2018-09-12 22:14:59 -0400

    first commit

--- Lab2/index.js
+++ Lab2/index.js
@@ -1,1 +1,5 @@
-console.log("hello");
+var http = require('http');
+var server = http.createServer(function (request, response) {response.writeHead(200, {"Content-Type": "text/plain"});
+response.end("Hello World\n");});
+server.listen(8000, '127.0.0.1');
+console.log("Server listening on: 127.0.0.1:8000");
